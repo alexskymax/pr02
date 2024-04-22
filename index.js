c998ef246281d1c8ad94b093d242ad558751a7ad
alexskymax <alex.skymax@gmail.com>
2024-04-22 09:17:15 +0500

    pr02-main:init_project

---
+++ index.js
@@ -0,0 +1,7 @@
+// JavaScript код для вашего сайта
+
+// Пример JavaScript кода:
+document.addEventListener('DOMContentLoaded', function() {
+    // Код JavaScript, который будет выполняться после загрузки документа
+    console.log('Документ загружен!');
+});
